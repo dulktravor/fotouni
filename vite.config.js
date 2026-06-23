fcb7657a7b9883fc6bb4aff235c02e04b0bdbf89
dulktravor <enrique.lazaro.dulktravor@gmail.com>
2026-06-22 23:40:18 -0500

    Configure GitHub Pages deployment via GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/fotouni/', // Set the base path for GitHub Pages deployment
   server: {
-    host: true, // Allow connections from local network (IP address)
-    port: 5173  // Set development server port
+    host: true,        // Allow connections from local network (IP address)
+    port: 5173         // Set development server port
   }
 });
